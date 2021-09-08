333f1f38a17f1f567a8095d647fda7f742e61a22
Anne_kj <746326366@qq.com>
2021-09-08 16:09:01 +0800

    add-heatmap

--- lib/heatmap.py
+++ lib/heatmap.py
@@ -8,16 +8,26 @@ import numpy as np
 
 from lib.constants import alphabet
 
-KEYBOARD_BACKGROUND_JPG = '../data/keyboard.svg.jpg'
-KEYBOARD_BACKGROUND_SVG = '../data/keyboard.svg'
+KEYBOARD_BACKGROUND = '../data/keyboard2.png'
+KEYBOARD_KEY_POSITIONS = [
+    (207, 371), (600, 454), (428, 453), (381, 373), (362, 274),
+    (468, 365), (550, 363), (642, 368), (774, 287), (728, 368),
+    (813, 368), (903, 366), (776, 452), (686, 446), (878, 283),
+    (966, 279), (182, 281), (445, 284), (302, 366), (524, 280),
+    (708, 276), (508, 446), (274, 283), (335, 450), (623, 289),
+    (259, 451)
+]
+ALPHA_POSITION = {}
+for i in range(len(alphabet)):
+    ALPHA_POSITION[alphabet[i]] = KEYBOARD_KEY_POSITIONS[i]
 
 
 # 创建一个半透明 colormap 并注册到 plt
 def register_coolwarm_alpha_colormap(alpha: float) -> str:
     ncolors = 256
     color_array = plt.get_cmap('coolwarm')(range(ncolors))
-    color_array[:,-1] = alpha
-    map_object = LinearSegmentedColormap.from_list(name='coolwarm_alpha',colors=color_array)
+    color_array[:, -1] = alpha
+    map_object = LinearSegmentedColormap.from_list(name='coolwarm_alpha', colors=color_array)
     plt.register_cmap(cmap=map_object)
     return 'coolwarm_alpha'
 
@@ -27,38 +37,30 @@ register_coolwarm_alpha_colormap(0.5)
 
 # 根据 xyz 坐标的散点图绘制热力图
 def draw_heatmap_scatter(x: list, y: list, z: list):
-    img = plt.imread(KEYBOARD_BACKGROUND_JPG)
-    height, width = img.shape[0], img.shape[1]
-    plt.imshow(img)
+    # 读图片的长宽
+    img = plt.imread(KEYBOARD_BACKGROUND)
+    height, width = img.shape[0:2]
 
     ip = interp2d(x, y, z, kind='cubic')
     x1 = np.arange(0, width)
     y1 = np.arange(0, height)
     z2 = ip(x1, y1)
-
-    plt.pcolormesh(x1, y1, z2, cmap='coolwarm_alpha')
-    plt.scatter(x, y)
+    x2, y2 = np.meshgrid(x1, y1)
+    fig, ax = plt.subplots()
+    ax.imshow(img)
+    im = ax.pcolormesh(x2, y2, z2, cmap='coolwarm_alpha')
+    ax.scatter(x, y)
+    fig.colorbar(im, orientation='vertical')
     plt.show()
-
-
-# 获取字母的坐标
-def get_alpha_position() -> dict:
-    # doc = lxml.etree.parse(KEYBOARD_BACKGROUND_SVG)
-    # for i in doc.getiterator():
-    #     print(i)
-    d = {}
-    for i in alphabet:
-        d[i] = [random()*1200, random()*300]
-    return d
+    print(1)
 
 
 # 根据字母频度绘制热力图
 def draw_heatmap(alpha_count: dict):
-    alpha_position = get_alpha_position()
     x, y, z = [], [], []
     for alpha in alphabet:
-        x.append(alpha_position[alpha][0])
-        y.append(alpha_position[alpha][1])
+        x.append(ALPHA_POSITION[alpha][0])
+        y.append(ALPHA_POSITION[alpha][1])
         z.append(alpha_count[alpha])
     draw_heatmap_scatter(x, y, z)
 
@@ -74,11 +76,9 @@ if __name__ == '__main__':
     # x = [0, 100, 200, 0, 100, 200]
     # y = [0, 0, 0, 300, 300, 300]
     # z = [1, 2, 3, 4, 5, 6]
-
     # draw_heatmap_scatter(x, y, z)
 
     d = {}
     for i in range(len(alphabet)):
-        d[alphabet[i]] = i
+        d[alphabet[i]] = (i+1)*100
     draw_heatmap(d)
-
